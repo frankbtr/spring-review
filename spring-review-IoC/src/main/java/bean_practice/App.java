5ffe6e6819caf6e46f308457f451d7e0caae07e9
Frank Batur <frank_batur@outlook.com>
2023-02-26 20:19:55 -0500

    Task-1 is done

---
+++ spring-review-IoC/src/main/java/bean_practice/App.java
@@ -0,0 +1,17 @@
+package bean_practice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        //how to create spring container?
+        ApplicationContext container = new AnnotationConfigApplicationContext(AppConfig.class);
+        FullTimeEmployee ft = container.getBean(FullTimeEmployee.class);
+        ft.createAccount();
+
+        PartTimeEmployee pt = container.getBean(PartTimeEmployee.class);
+        pt.createAccount();
+    }
+}
